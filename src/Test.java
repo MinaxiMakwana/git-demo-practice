8ebb7261e00f58a952e4aea1c0b1b3bab97823e9
MinaxiMakwana <147212536+MinaxiMakwana@users.noreply.github.com>
2024-01-27 15:09:42 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Mina");
+        System.out.println("My name is Mina, How are you");
     }
 }
