58ad646fc1e089284f8d0fa1be70aeafd1df5183
Minaxi Makwana <Minaxi.Makwana139@gmail.com>
2024-01-27 15:14:10 +0000

    commit3

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("My name is Mina");
+        System.out.println("hello");
         System.out.println("My name is Mina, How are you");
     }
 }
